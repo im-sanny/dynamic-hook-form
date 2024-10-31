d1ecc32e625f941063629e9a35fcded950b17a09
im-sanny <raselparvezsanny@gmail.com>
2024-10-31 20:59:43 +0600

    add: logic and input field added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import { TextField } from "@mui/material";
+import { Button, Checkbox, FormControlLabel, TextField } from "@mui/material";
 import { Container } from "./Container";
+import { FieldError, SubmitHandler, useForm, useWatch } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formDefaultValues, formSchema, FormSchema } from "./formSchema";
 
 function App() {
+  const {
+    register,
+    formState: { errors },
+    control,
+    handleSubmit,
+  } = useForm<FormSchema>({
+    mode: 'all',
+    resolver: zodResolver(formSchema),
+    defaultValues: formDefaultValues,
+  });
+
+  const fullError: FieldError<formSchema/> = errors
+
+  const hasWorkExperience = useWatch({ control, name: "hasWorkExperience" });
+
+  const onSubmit: SubmitHandler<FormSchema> = (data) => {
+    alert(JSON.stringify(data, null, 2))
+  }
   return (
     <Container>
-      <TextField label="Full Name"></TextField>
+      <TextField {...register('fullName')}></TextField>
+      <FormControlLabel {...register('hasWorkExperience')}
+        label='Work Experience?'
+        control={<Checkbox />}
+      />
+      {hasWorkExperience &&
+        <TextField
+          {...register("companyName")}
+          label="Company Name"
+          helperText={errors.companyName?.message}
+          error={!!errors.companyName}
+        />
+      }
+      <Button variant="contained" onClick={handleSubmit(onSubmit)}>
+        Submit
+      </Button>
     </Container>
   )
 }
--- src/formSchema.ts
+++ src/formSchema.ts
@@ -1,13 +1,24 @@
 import { z } from 'zod';
 
+const workExperienceScheme = z.discriminatedUnion('hasWorkExperience', [
+    z.object({
+        hasWorkExperience: z.literal(true),
+        companyName: z.string().min(1),
+    }),
+    z.object({
+        hasWorkExperience: z.literal(false)
+    })
+]);
+
 const formSchema = z.object({
     fullName: z.string().min(1),
-})
+}).and(workExperienceScheme);
 
 type FormSchema = z.infer<typeof formSchema>;
 
 const formDefaultValues: FormSchema = {
     fullName: "",
+    hasWorkExperience: false,
 }
 
-export {formDefaultValues, formSchema, type FormSchema}
+export { formDefaultValues, formSchema, type FormSchema }
