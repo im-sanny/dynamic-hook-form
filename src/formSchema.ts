169765950ef3a21e672bad6d8e1a584df94ed3e0
im-sanny <raselparvezsanny@gmail.com>
2024-11-01 16:02:21 +0600

    add: dynamic language schema added

--- src/formSchema.ts
+++ src/formSchema.ts
@@ -10,15 +10,31 @@ const workExperienceScheme = z.discriminatedUnion('hasWorkExperience', [
     })
 ]);
 
+const languageKnowledgeSchema = z.discriminatedUnion('knowsOtherLanguages', [
+    z.object({
+        knowsOtherLanguages: z.literal(true),
+        languages: z.array(
+            z.object({
+                name: z.string().min(1),
+            })
+        )
+    }),
+    z.object({
+        knowsOtherLanguages: z.literal(false)
+    })
+])
+
 const formSchema = z.object({
     fullName: z.string().min(1),
-}).and(workExperienceScheme);
+}).and(workExperienceScheme)
+.and(languageKnowledgeSchema)
 
 type FormSchema = z.infer<typeof formSchema>;
 
 const formDefaultValues: FormSchema = {
     fullName: "",
     hasWorkExperience: false,
+    knowsOtherLanguages: false,
 }
 
 export { formDefaultValues, formSchema, type FormSchema }
